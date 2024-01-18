47bdb842c95dd0ddbaf183216ef876502077cfe2
YsisC <ysisclongart@gmail.com>
2024-01-17 21:32:22 -0300

    add funtions to clean code

--- mybooks_postgres.py
+++ mybooks_postgres.py
@@ -6,7 +6,7 @@ import json
 from decouple import config  
 import requests
 
-def obtener_datos(url):
+def get_data(url):
     try:
         response = requests.get(url)
         response.raise_for_status() 
@@ -24,12 +24,12 @@ url_api2 = config('URL_API_2')
 
 # Utilizando ThreadPoolExecutor para realizar las solicitudes en paralelo
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    resultados = list(executor.map(obtener_datos, [url_api1, url_api2]))
+    results = list(executor.map(get_data, [url_api1, url_api2]))
    
 
 
-with open('resultados.json', 'w') as archivo:
-    json.dump(resultados, archivo)
+with open('results.json', 'w') as archivo:
+    json.dump(results, archivo)
 
 
 # Conectar a la base de datos
@@ -38,19 +38,24 @@ conexion = pyo.connect(**dbConfig)
 cursor = conexion.cursor()
 
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS resultados (
+    CREATE TABLE IF NOT EXISTS results (
         id SERIAL PRIMARY KEY,
         data JSONB
     )
 """)
 
-   
-# Insertar los resultados en la tabla
-for resultado in resultados:
-    print(f" resultado {resultado}")
-    cursor.execute("INSERT INTO resultados (data) VALUES (%s)", (json.dumps(resultado),))
-    
+def view():
+    cursor.execute("SELECT * FROM results")
+    rows = cursor.fetchall()
+    return rows
 
+def insert_results(list):
+    for result in results:
+        # print(f" resultado {resultado}")
+        cursor.execute("INSERT INTO results (data) VALUES (%s)", (json.dumps(result),))
+
+insert_results(results)
+print(view())
 
 conexion.commit()
 conexion.close()
