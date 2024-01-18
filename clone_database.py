75502da43245d1a3f299db1aedd7777573644e13
YsisC <ysisclongart@gmail.com>
2024-01-18 13:09:45 -0300

    fix bug in classBook

--- clone_database.py
+++ clone_database.py
@@ -16,7 +16,7 @@ class Bookdb:
         self.url_api1 = url_api1
         self.url_api2 = url_api2
         self.con = pyo.connect(**dbConfig)
-        self.cursor = con.cursor()
+        self.cursor = self.con.cursor()
         
         print("You have connected to the  database")
         print(con)
@@ -42,30 +42,31 @@ class Bookdb:
             results = list(executor.map(self.get_data, [self.url_api1, self.url_api2]))
         return results
     
-    def save_results_to_file(self, results, filename='results.json'):
+    def save_results_to_file(self, results, filename='results_replicate.json'):
         with open(filename, 'w') as archivo:
             json.dump(results, archivo)
 
     def create_database_table(self):
         self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS results (
+            CREATE TABLE IF NOT EXISTS results_replicate (
                 id SERIAL PRIMARY KEY,
                 data JSONB
             )
         """)
-
         self.con.commit()
+
     def view(self):
-        self.cursor.execute("SELECT * FROM results")
+        self.cursor.execute("SELECT * FROM results_replicate")
         rows = self.cursor.fetchall()
         print(rows)
         return rows
         
-    def insert(self,results):
-        for item in results:
-            self.cursor.execute("INSERT INTO results (data) VALUES (%s)", (json.dumps(item),))
+    def insert(self,list):
+        for item in list:
+            self.cursor.execute("INSERT INTO results_replicate (data) VALUES (%s)", (json.dumps(item),))
        
         self.con.commit()
+        print('Creado')
     
 db = Bookdb(
         url_api1=config('URL_API_1'),
@@ -73,6 +74,7 @@ db = Bookdb(
     )
 
 results= db.fetch_data_parallel()
+
 db.create_database_table()
 db.insert(results)
 db.view()
